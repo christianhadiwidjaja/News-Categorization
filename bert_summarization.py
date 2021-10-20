9a8927b7607bea17f6e2850efa5163bd1ec79c4c
Christian Imanuel Hadiwidjaja <84717225+christianhadiwidjaja@users.noreply.github.com>
2021-10-21 03:14:58 +0700

    Delete bert_summarization.py

--- bert_summarization.py
+++
@@ -1,8 +0,0 @@
-from summarizer import Summarizer
-
-def bert_summarizer(raw_text):
-    model = Summarizer()
-    result = model(raw_text, min_length=20)
-    summary = "".join(result)
-
-    return summary
